7784ac3431bce583d622c6ddefd98b4d03d30171
ketankore23 <ketan.kore@sunbeaminfo.com>
2023-04-14 18:34:22 +0530

    day1

---
+++ Day1/codes/main.c
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+
+int main( void )
+{
+    printf("Hello world\n");  
+    return 0; 
+}
+/*
+
+    multiline comment ( block comment ) => explaination  
+
+    Powershell:  
+    1. gcc main.c 
+    2. ./a.exe 
+    3. clear => to clear the screen 
+
+    cmd:  
+    1. gcc main.c 
+    2. a.exe 
+    3. cls => to clear the screen 
+
+    bash
+    1. gcc main.c 
+    2. ./a.exe 
+    3. clear => to clear the screen 
+*/
